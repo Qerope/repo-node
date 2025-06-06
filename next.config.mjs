b6b8485dc3093edc7b0ea70240f1aae12d77adba
Qerope <preloopadm@gmail.com>
2025-06-06 19:26:31 -0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/repo-node',
-  assetPrefix: '/repo-node/',
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
